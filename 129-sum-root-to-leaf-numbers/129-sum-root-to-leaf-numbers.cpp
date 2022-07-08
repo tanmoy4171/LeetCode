ba3708176ee77f5063ab2bbfe2a6c2c2a930ccaf
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-07-08 15:34:09 +0530

    Time: 5 ms (33.34%), Space: 9.3 MB (46.75%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -9,20 +9,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Approach--->1
 class Solution {
 public:
     int sumNumbers(TreeNode* root) {
         return helperSumNumbers(root,0);
     }
 private:
-     int helperSumNumbers(TreeNode* root,int sum){
-        if(!root){
-            return 0; 
-        }
-        sum=(sum*10)+root->val; 
-        if (!root->left && !root->right){
-            return sum;
-        }  
-        return helperSumNumbers(root->left,sum)+helperSumNumbers(root->right,sum);
+    int helperSumNumbers(TreeNode* root,int num){
+        return root->left == root->right ? num * 10 + root->val :
+            ((root->left ? helperSumNumbers(root->left, num * 10 + root->val) : 0) + 
+            (root->right ? helperSumNumbers(root->right, num * 10 + root->val) : 0));
     }
 };
+//Approach-->2
+// class Solution {
+// public:
+//     int sumNumbers(TreeNode* root) {
+//         return helperSumNumbers(root,0);
+//     }
+// private:
+//      int helperSumNumbers(TreeNode* root,int sum){
+//         if(!root){
+//             return 0; 
+//         }
+//         sum=(sum*10)+root->val; 
+//         if (!root->left && !root->right){
+//             return sum;
+//         }  
+//         return helperSumNumbers(root->left,sum)+helperSumNumbers(root->right,sum);
+//     }
+// };
