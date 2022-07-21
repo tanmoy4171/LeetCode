7427e7df57ced004f14b3eaf4e75426d42a663e9
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-07-21 14:37:36 +0530

    Time: 4 ms (51.96%), Space: 7.4 MB (63.80%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -11,20 +11,11 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        vector<int>v;
-        ListNode* left_node = head; 
-        for(int i = 1; i < left; i++) left_node = left_node->next;
-        
-        ListNode* node = left_node;
-        for(int i = 0; i < (right-left+1); i++){
-            v.push_back(node->val);
-            node = node->next;
-        }
-        reverse(v.begin(), v.end());
-        for(int i = 0; i < v.size(); i++){
-            left_node->val = v[i];
-            left_node = left_node->next;
-        }
-    return head;         
+        auto **pivot{&head->next}, **start{&head};
+
+        for(; --right && --left; pivot = &(*(start = pivot))->next); 
+        for(; right--; swap(*start, (*pivot)->next), swap(*start, *pivot)); 
+
+        return head;
     }
 };
