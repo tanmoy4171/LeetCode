7abe21ca60706b8a546063e874f7f067f1548dfc
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-04 01:21:02 +0530

    Time: 180 ms (43.59%), Space: 38.9 MB (48.99%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,61 +1,13 @@
-class Node{
-    public:
-    int startDate,endDate;
-    Node*left;
-    Node*right;
-    Node(int startDate,int endDate){
-        this->startDate=startDate;
-        this->endDate=endDate;
-        this->left=NULL;
-        this->right=NULL;
-    }
-};
 class MyCalendar {
-private:
-    Node*root;
-    bool _book(Node*node,int start,int end){
-        if(!node){
-            return true;
-        } else if(node->endDate<=start){
-            if(!node->right){
-                return true;
-            } else {
-                return this->_book(node->right,start,end);
-            }
-        } else if(node->startDate>=end){
-            if(!node->left){
-                return true;
-            } else {
-                return this->_book(node->left,start,end);
-            }
-        } else {
-            return false;
-        }
-    }
-    Node*_update(Node*node,int start,int end){
-        if(!node){
-            node=new Node(start,end);
-            return node;
-        } else {
-            if(node->endDate<=start){
-                node->right=this->_update(node->right,start,end);
-            } else {
-                node->left=this->_update(node->left,start,end);
-            }
-            return node;
-        }
-    }
 public:
-    MyCalendar() {
-        this->root=NULL;
+    map<int,int>events;
+    MyCalendar() {   
     }
-    
     bool book(int start, int end) {
-        bool ans=this->_book(this->root,start,end);
-        if(ans){
-            this->root=this->_update(this->root,start,end);
-        }
-        return ans;
+        auto next = events.upper_bound(start);
+        if(next != events.end() &&   (*next).second < end)return false;
+        events.insert({end,start});
+        return true;
     }
 };
 
