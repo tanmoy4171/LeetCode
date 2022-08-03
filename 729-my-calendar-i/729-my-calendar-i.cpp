5b9c3595346a9083a572d6e2182c04229ddc866f
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-04 01:20:11 +0530

    Time: 140 ms (67.78%), Space: 47.6 MB (6.38%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,22 +1,66 @@
+class Node{
+    public:
+    int startDate,endDate;
+    Node*left;
+    Node*right;
+    Node(int startDate,int endDate){
+        this->startDate=startDate;
+        this->endDate=endDate;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
 class MyCalendar {
+private:
+    Node*root;
+    bool _book(Node*node,int start,int end){
+        if(!node){
+            return true;
+        } else if(node->endDate<=start){
+            if(!node->right){
+                return true;
+            } else {
+                return this->_book(node->right,start,end);
+            }
+        } else if(node->startDate>=end){
+            if(!node->left){
+                return true;
+            } else {
+                return this->_book(node->left,start,end);
+            }
+        } else {
+            return false;
+        }
+    }
+    Node*_update(Node*node,int start,int end){
+        if(!node){
+            node=new Node(start,end);
+            return node;
+        } else {
+            if(node->endDate<=start){
+                node->right=this->_update(node->right,start,end);
+            } else {
+                node->left=this->_update(node->left,start,end);
+            }
+            return node;
+        }
+    }
 public:
-    map<int, int> mp;
     MyCalendar() {
-        
+        this->root=NULL;
     }
     
     bool book(int start, int end) {
-        mp[start]++;
-        mp[end]--;
-        int sum=0;
-        for(auto it=mp.begin(); it!=mp.end(); it++) {
-            sum += it->second;
-            if(sum>1){
-                mp[start]--;
-                mp[end]++;
-                return false;
-            }
+        bool ans=this->_book(this->root,start,end);
+        if(ans){
+            this->root=this->_update(this->root,start,end);
         }
-        return true;
+        return ans;
     }
 };
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
