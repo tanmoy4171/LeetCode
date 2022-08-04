f08d0c23eed42c174fa66445b35d17a5d8b84ae6
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-05 02:01:39 +0530

    Time: 28 ms (77.33%), Space: 18 MB (74.07%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
+    bool isValid(vector<vector<char>> &board,int row,int col){
+        char ch=board[row][col];
+        for(int i=0;i<9;i++){
+            if(i!=row){
+                if(board[i][col]==ch)
+                    return false;
+            }
+        }
+        for(int j=0;j<9;j++){
+            if(j!=col){
+                if(board[row][j]==ch)
+                    return false;
+            }
+        }
+        int k=(row/3)*3;
+        int l=(col/3)*3;
+        for(int i=k;i<k+3;i++){
+            for(int j=l;j<l+3;j++){
+                if(i!=row && j!=col){
+                    if(board[i][j]==ch)
+                        return false;
+                }
+            }
+        }
+        return true;
+        
+    }
+    
     bool isValidSudoku(vector<vector<char>>& board) {
-        int rows[9][10];
-        int cols[9][10];
-        int boxes[9][10];
-        memset(rows, 0, sizeof(rows));
-        memset(cols, 0, sizeof(cols));
-        memset(boxes, 0, sizeof(boxes));
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    int num = board[i][j] - '0';
-                    if (++rows[i][num] > 1 || ++cols[j][num] > 1 || ++boxes[(i/3)*3 + j/3][num] > 1) return false;
-                
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]!='.'){
+                    if(isValid(board,i,j)==false)
+                        return false;
                 }
             }
         }
