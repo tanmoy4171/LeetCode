9c23a567998c3733485712df21bc0a175bd34a8d
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-05 01:48:34 +0530

    Time: 3 ms (94.49%), Space: 9.6 MB (94.52%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-        // int l=0,r=nums.size()-1,mid,ans;
-        // if(target>nums[r]){
-        //     return r+1;
-        // }
-        // while(l<=r){
-        //     mid=l+(r-1)/2;
-        //     if(nums[mid]>=target){
-        //         ans=mid;
-        //         r=mid-1;
-        //     }else{
-        //         l=mid+1;
-        //     }
-        // }
-        // return ans;
+       // return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        int n=nums.size();
+    int s=0;
+    int e=n-1;
+    
+    int mid=s+(e-s)/2;
+    
+while(s<=e)
+    {
+    
+        if(nums[mid]<target)
+            s=mid+1;
+        
+        else
+        e=mid-1;
+    
+        mid=s+(e-s)/2;
+    }
+    
+    return mid;
     }
 };
