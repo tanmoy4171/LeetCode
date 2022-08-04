e4ff1a6853e93754bce0bad8a060b08da0bc4425
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-05 01:32:34 +0530

    Time: 15 ms (88.36%), Space: 13.2 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int rowsize=matrix.size(); 
-        int columnsize=matrix[0].size(); 
-        vector<int>rowmarker(rowsize,1);
-        vector<int>columnmarker(columnsize,1); 
-        for(int i=0;i<rowsize;i++)
-        {
-            for(int j=0;j<columnsize;j++)
-            {
+        int col0=1, rows=matrix.size(), columns=matrix[0].size();
+        for(int i=0 ; i<rows ; i++){
+            if(matrix[i][0]==0) col0=0;
+            for(int j=1 ; j<columns ; j++){
                 if(matrix[i][j]==0)
-                {
-                    rowmarker[i]=0; 
-                    columnmarker[j]=0;
+                    matrix[i][0]=matrix[0][j]=0;
+            }
+        }
+        for(int i=rows-1 ; i>=0 ; i--){
+            for(int j=columns-1 ; j>=1 ; j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
                 }
             }
-        } 
-        for(int i=0;i<rowsize;i++)
-        {
-            for(int j=0;j<columnsize;j++)
-            {
-                if(rowmarker[i]==0 or columnmarker[j]==0)
-                    matrix[i][j]=0;
-             }
+            if(col0==0)
+                matrix[i][0]=0;
         }
     }
 };
