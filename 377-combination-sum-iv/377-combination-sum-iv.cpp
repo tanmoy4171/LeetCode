e1a59f3b861f9d7764ef2721c15f3b25d77f248b
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-05 19:46:47 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (91.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> sumArray(target+1, -1);
-        sumArray[0]=1;
-        countSum(nums, target, sumArray);
-        return sumArray[target];
-    }
-    int countSum(vector<int>& nums, int target, vector<int>& sumArray) {
-        if(sumArray[target]>-1)
-            return sumArray[target];
-        int sum=0;
-        for(int i=0 ; i<nums.size(); i++) {
-            if(nums[i] <= target)
-                sum += countSum(nums, target-nums[i], sumArray);
-                // 4 -> (1, [3])
-                // 3 -> (2, [2])
-                // 2 -> (1, [1])
+        // support variables
+        long dp[target + 1];
+        dp[0] = 1;
+        // populating dp
+        for (int i = 1; i <= target; i++) {
+            // setting the initial value of a cell to 0
+            dp[i] = 0;
+            // updating dp[i] with all the previous combinations we can reach from there
+            for (int n: nums) {
+                if (i >= n) dp[i] += dp[i - n];
+                if (dp[i] > INT_MAX) break;
+            }
         }
-        sumArray[target] = sum;
-        return sum;
+        return dp[target];   
     }
 };
