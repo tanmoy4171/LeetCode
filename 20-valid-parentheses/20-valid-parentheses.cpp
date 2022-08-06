6c8e2c8410f80780a125526c60aa8425d25ee31b
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-07 00:05:44 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (50.94%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for(auto i:s){
-            if(i=='(' or i=='{' or i=='[') st.push(i);  
-            else  {
-                if(st.empty() or (st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or (st.top()=='[' and i!=']')) {
-                    return false;
-                }
-                st.pop();  
-            }
-        }
-        return st.empty();  
+        stack<char> sack;
+         int n = s.size();
+         for(int i=0;i<n;i++)
+         {   
+             if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+             { 
+               sack.push(s[i]);
+               continue;
+             }
+             if(sack.empty())
+                 return false;
+             char x = sack.top();
+             if((x=='(' && s[i]==')') || (x=='{' && s[i]=='}') || (x=='[' && s[i]==']'))
+                 sack.pop();
+             else
+                return false;
+         } 
+         if(sack.empty())
+             return true;
+         return false;
     }
 };
