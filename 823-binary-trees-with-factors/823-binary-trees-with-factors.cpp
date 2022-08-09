990503f5f12d8fee63d53954d62a2745a900e02e
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-09 13:38:15 +0530

    Time: 37 ms (98.46%), Space: 10.1 MB (46.46%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
-    int numFactoredBinaryTrees(vector<int>& A) {
-        vector<long long> dp(A.size(),1);
-        sort(A.begin(),A.end());
-        const int tt=1000000000+7;
-        unordered_map<int,int> ms;
-        for(int i=0;i<A.size();i++)
-        {
-            ms[A[i]]=i;
-            for(int j=i-1;j>=0;j--) 
-            {
-                if(A[i]%A[j]==0)
-                {
-                    int t=A[i]/A[j];
-                    if(ms.count(t)) dp[i]+=(dp[j]*dp[ms[t]])%tt;
-                }
-            }
+  int numFactoredBinaryTrees(vector<int>& arr) {
+    int mod = 1000000007, n = arr.size();
+    unsigned long long answer = 0;
+
+    sort(arr.begin(),arr.end());
+    vector<unsigned long long> dp(n);
+    unordered_map<int,int> mp;
+    vector<vector<tuple<int,int,int>>*> table(n, NULL);
+    
+    for (int i = 0; i != n; i++) mp[arr[i]] = i;
+
+    int i = 0;
+    while(true){
+      if(arr[i] > 31622) break; 
+      int squared = arr[i]*arr[i];
+      if(mp.count(squared)){
+        if(table[mp[squared]] == NULL)
+          table[mp[squared]] = new vector<tuple<int,int,int>>;
+        table[mp[squared]]->push_back({i,i,0});
+      }
+      if(squared >= arr.back()) break;
+      
+      int j = i + 1;
+      while(true){
+        unsigned long long num = (unsigned long long)(arr[i])*arr[j];
+        if(num > arr.back()) break;
+        if(mp.count(num)){
+          if(table[mp[num]] == NULL)
+            table[mp[num]] = new vector<tuple<int,int,int>>;
+          table[mp[num]]->push_back({i,j,1});
         }
-        long long ans=0;
-        ans=accumulate(dp.begin(),dp.end(),0LL);
-        return ans%tt;//%tt+7;
+        j++;
+      }
+      i++;
     }
+    
+    for(int i = 0; i != n; i++){
+      dp[i] = 1;
+      if(table[i])
+        for(int w = 0; w != table[i]->size(); w++)
+          dp[i] += (dp[get<0>(table[i]->operator[](w))]*dp[get<1>(table[i]->operator[](w))]<<
+                   get<2>(table[i]->operator[](w)));
+    }
+                
+    for (unsigned long long i : dp) answer += i;
+    return (int)(answer % mod);    
+  }
 };
