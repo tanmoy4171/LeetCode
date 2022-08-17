7a889c6a4044e4c12f610500d4d28e3d5358c759
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-17 22:13:11 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (61.19%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    string morse[26] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-    string convertToMorse(string s) {
-        string res;
-        for (char c: s) res.append(morse[c - 'a']);
-        return res;
-    }
     int uniqueMorseRepresentations(vector<string>& words) {
-        unordered_set<string> seen;
-        for (string s: words) seen.insert(convertToMorse(s));
-        return seen.size();
+        vector<string> a = {".-","-...","-.-.","-..",".","..-.","--.","....",
+                  "..",".---","-.-",".-..","--","-.","---",".--.",
+                  "--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        unordered_set<string> u;
+
+        for (auto word : words) {
+            string t;
+            for (auto c : word) t += a[c - 'a'];
+            u.insert (t);
+         }
+
+      return u.size ();
     }
 };
