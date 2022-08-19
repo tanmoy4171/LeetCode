e8a89c19897960b337d63108aadeef46868c5240
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-19 20:09:12 +0530

    Time: 8 ms (46.18%), Space: 12.2 MB (36.78%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        // Optimal Solution Time O(N) & Auxiliary Space O(1)
-	    // Narayana Panditha's Algorithm
-        int len=nums.size(), k=0, l=0;
-        for(k=len-2;k>=0;k--){
-            if(nums[k]<nums[k+1])
-                break;
+	    // // Optimal Solution Time O(N) & Auxiliary Space O(1)
+	    // // Narayana Panditha's Algorithm
+	    // int len=nums.size(), k=0, l=0;
+	    // for(k=len-2;k>=0;k--){
+	    // if(nums[k]<nums[k+1])
+	    // break;
+	    // }
+	    // if(k<0){
+	    // reverse(nums.begin(),nums.end());
+	    // }
+	    // else{
+	    // for(l=len-1;l>k;l--){
+	    // if(nums[l]>nums[k])
+	    // break;
+	    // }
+	    // swap(nums[k],nums[l]);
+	    // reverse(nums.begin()+k+1,nums.end());
+	    // }
+        // support variables
+        int i = nums.size() - 2, j = i + 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+        // edge case: already the highest permutation you can get
+        if (i == -1) {
+            reverse(begin(nums), end(nums));
+            return;
         }
-        if(k<0){
-            reverse(nums.begin(),nums.end());
-        }
-        else{
-            for(l=len-1;l>k;l--){
-                if(nums[l]>nums[k])
-                    break;
-        }
-            swap(nums[k],nums[l]);
-            reverse(nums.begin()+k+1,nums.end());
-        }
+        // finding the first element > nums[i]
+        while (nums[--j] <= nums[i]);
+        // swapping i and j's values
+        swap(nums[i], nums[j]);
+        // ordering the rest
+        sort(begin(nums) + i + 1, end(nums));
     }
 };
