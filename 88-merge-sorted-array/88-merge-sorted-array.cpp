1d7e7b36107452f4fd627bbb1518d2970fa02688
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-19 20:52:44 +0530

    Time: 7 ms (29.44%), Space: 9.1 MB (72.88%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m-1;
-        int j = n-1;
+        //Size of nums1 including 0's
         int k=m+n-1;
-        while(j>=0){
-            if(i>=0&&nums1[i]>nums2[j]){
-                nums1[k--] = nums1[i--];
-            }else{
-                nums1[k--] = nums2[j--];
-            }
+		
+		//Both the pointers to point at last index
+        m--;
+        n--;
+		
+        while(m>=0 && n>=0) {
+			//Comparing the values and adjusting the k value accordingly
+            if(nums1[m]>nums2[n]) nums1[k--]=nums1[m--];
+            else nums1[k--]=nums2[n--];
         }
+		
+		//It might happen that elements of second array are smaller than smallest element of first array
+        while(n>=0) nums1[k--]=nums2[n--];
+		
+		//We need not cover the otherwise case of first array elements begin smaller, because they are already in first half of the array
     }
 };
