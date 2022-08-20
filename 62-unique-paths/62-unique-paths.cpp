089dc6b0b1bc47faf32772f12b954d769178c45f
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-21 03:08:40 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.81%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(n, vector<int>(m, 1));
-
-        for (int i = 1; i < n; i++)
-            for (int j = 1; j < m; j++)
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        // Create a 2d vector for m*n and set value to 1;
+		
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+		
+        // Many may confuse by seeing the above line 
+        // This is same as dp[m][n] = {0} using array
         
-        return dp[n-1][m-1];
+        // iterate to all the grids in m*n and set dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        // Finally result the last grid ans
+        return dp[m - 1][n - 1];
     }
 };
