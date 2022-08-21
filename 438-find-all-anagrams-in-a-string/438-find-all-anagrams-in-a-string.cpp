ff6b4dae66ac8bb62c23af85cf4a57b15591101a
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-22 00:39:39 +0530

    Time: 9 ms (97.22%), Space: 8.8 MB (39.63%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        if(s.length() < p.length()) return {};
-            vector<int> mp_p(26), mp_s(26), res;
-            for(char c : p) ++mp_p[c-'a'];
-            for(int i = 0; i < p.length()-1; ++i) ++mp_s[s[i]-'a'];
-            for(int i = 0, j = p.length()-1; j < s.length(); ++i, ++j)
-            {
-                ++mp_s[s[j]-'a'];
-                if(mp_p == mp_s) res.push_back(i);
-                --mp_s[s[i]-'a'];
-            }
-            return res;
+        if(s.length()<p.length())       return {};
+        
+        vector<int> ans;
+        vector<int> pMap(26,0);
+        vector<int> window(26,0);
+        
+        int windStart = 0, windEnd = p.size()-1;
+        
+        for(auto c: p)  pMap[c - 'a']++;
+		
+        for(int i = 0; i <= windEnd; i++)           // First Window
+            window[s[i] - 'a']++;
+        
+        if(window == pMap)  
+			ans.push_back(0);
+        
+        windEnd++;              // Increment so we can include next element in our window.
+        
+        while(windEnd < s.size()){
+            
+            window[s[windEnd++] - 'a'] += 1;        //Include
+            window[s[windStart++] - 'a'] -= 1;      //Exclude
+            
+            if(window == pMap)                      // O(26) = O(1)
+                ans.push_back(windStart);
+        }
+        return ans;
     }
 };
