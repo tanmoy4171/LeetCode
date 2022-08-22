f29da583290ae61ebcba9ccdd68ca9e74a27ff22
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-22 23:47:24 +0530

    Time: 4 ms (40.16%), Space: 5.9 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
-    bool isPowerOfFour(int num) {
-       return num > 0 && (num & (num - 1)) == 0 && (num & 0xAAAAAAAA) == 0;
+    bool isPowerOfFour(int n) {
+       // return num > 0 && (num & (num - 1)) == 0 && (num & 0xAAAAAAAA) == 0;
+        if(n ==  1){
+            return true;
+        }
+        if(n == 0){
+            return false;
+        }
+        if(n % 4 != 0){
+            return false;
+        } 
+        
+        return isPowerOfFour(n/4);
     }
 };
