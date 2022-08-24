2c657569cf52915048ae5ec12c90a9be562c7c70
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-24 21:50:47 +0530

    Time: 38 ms (40.21%), Space: 6.1 MB (7.72%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        return n < 1 ? false : !(1162261467 % n);
+        if(n == 0 || n < 1) return false;
+        for(int i = 0; i < n; i++)
+        {
+            long long int value = pow(3,i);
+            if(value == n) return true;
+            else if(value > n) return false;
+        }
+        return true;
     }
 };
