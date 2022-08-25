7a0e05094356e719f6b2f57d87030423b38aeb01
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-25 22:02:06 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.25%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
-    int findx(vector<int> &dp,int n){
-        if(n==1) return dp[n] = 1;
-        if(n==2) return dp[n] = 2;
-        
-        if(dp[n]!=-1) return dp[n];
-        int ans = 0;
-        for(int i=1;i<=n;i++){
-            ans += (findx(dp,i-1)*findx(dp,n-i));
-        }
-       return dp[n] = ans;
-    }
     int numTrees(int n) {
-        vector<int> dp(n+1,-1);
-        dp[0] = 1;
-        return findx(dp,n);
+        long long ans=1;
+        for(int i=1;i<=n;i++){
+            ans=ans*(n+i)/i;
+        }
+        return ans/(n+1);
     }
 };
