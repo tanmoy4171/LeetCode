f576887544ab9a03de9e807a529781b2ed7fda27
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-08-29 20:24:13 +0530

    Time: 79 ms (19.02%), Space: 12.6 MB (35.81%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
-    void DFS(vector<vector<char>>& grid, int i, int j) {
-        // boundary checking
-        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size())
-            return;
-        // return if current position is of water or is already visited
-        if(grid[i][j] == '2' || grid[i][j] == '0')
-            return;
-        
-        // mark the current as visited
-        grid[i][j] = '2';
-        
-        // do DFS in all 4 directions
-        DFS(grid, i+1, j);
-        DFS(grid, i, j-1);
-        DFS(grid, i-1, j);
-        DFS(grid, i, j+1);
+    void dfs(int i,int j,vector<vector<bool>>&vis,vector<vector<char>>&grid)
+    {
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || vis[i][j]==1 || grid[i][j]!='1')
+            return ;
+        vis[i][j]=1;
+        dfs(i+1,j,vis,grid);
+        dfs(i-1,j,vis,grid);
+        dfs(i,j+1,vis,grid);
+        dfs(i,j-1,vis,grid);
     }
-    
     int numIslands(vector<vector<char>>& grid) {
-        // We can treat the matrix grid as a grid. Each Island is a
-        // connected component. The task is to find no. of disconnectedd components
-        // in the graph.
-        
-        int islands = 0;
-        // We make each 1 as 2 in when it is visited
-        for(int i = 0; i < grid.size(); i++) {
-            for(int j = 0; j < grid[0].size(); j++) {
-                // do DFS in case has not been visited and there is land
-                if(grid[i][j] == '1') {
-                    DFS(grid, i, j);
-                    ++islands;
-                } 
+        int c=0;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<bool>> vis(n,vector<bool>(m,0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(vis[i][j]==0 && grid[i][j]=='1')
+                {
+                    dfs(i,j,vis,grid);
+                    c++;
+                }
             }
         }
-        return islands;
+        return c;
     }
 };
