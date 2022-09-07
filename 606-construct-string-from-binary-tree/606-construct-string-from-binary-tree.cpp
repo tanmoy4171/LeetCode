3e53d5188a2ccba905476cf4865b93eaaeb531c5
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-09-07 22:28:35 +0530

    Time: 18 ms (92.70%), Space: 26 MB (50.25%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
@@ -11,20 +11,11 @@
  */
 class Solution {
 public:
-	string tree2str(TreeNode* root) {
-
-		string s = to_string(root->val);
-
-		if(root->left){
-			s += "(" + tree2str(root->left) + ")";
-		}
-
-		if(root->right){
-			if(!root->left){ //left not present, but right present
-				s += "()";
-			}
-			s += "(" + tree2str(root->right) + ")";
-		}
-		return s;
-	}
+    string tree2str(TreeNode* root) {
+        if (!root) return "";
+        if (!root->left && !root->right) return to_string(root->val);
+        if (!root->right)
+            return to_string(root->val) + "(" + tree2str(root->left) + ")";
+        return to_string(root->val) + "(" + tree2str(root->left) + ")(" + tree2str(root->right) + ")";
+    }
 };
