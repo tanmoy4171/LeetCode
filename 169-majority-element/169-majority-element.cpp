37a8e1d59e4df6ca4277ed35074215c323c57216
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-10-04 20:54:54 +0530

    Time: 39 ms (41.64%), Space: 19.3 MB (99.57%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int candidate=0;
+        for(int it:nums){
+            if(count==0) candidate=it;
+            if(it==candidate) count++;
+            else count--;
+        }
+        return candidate;
+    }
+};
