777f6d0429efb19d3d6d32114f6453fda1790e66
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2022-10-14 05:45:19 +0530

    Time: 2245 ms (13.71%), Space: 294.9 MB (20.46%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -11,18 +11,21 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        if(!head->next){
-            delete head;  /*this is important to do*/
-            return NULL;
+        // Edge case: return nullptr if there is only one node.
+        if (head -> next == nullptr)
+            return nullptr;
+        
+        // Initialize two pointers, 'slow' and 'fast'.
+        ListNode *slow = head, *fast = head -> next -> next;
+        
+        // Let 'fast' move forward by 2 nodes, 'slow' move forward by 1 node each step.
+        while (fast != nullptr && fast -> next != nullptr) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
         }
-        ListNode *slow=head,*fast=head,*prev;
-        while(fast && fast->next){
-            prev=slow;
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        prev->next=slow->next;
-        delete slow;         /*this is important to do*/
+        
+        // When 'fast' reaches the end, remove the next node of 'slow' and return 'head'.
+        slow -> next = slow -> next -> next;
         return head;
     }
 };
