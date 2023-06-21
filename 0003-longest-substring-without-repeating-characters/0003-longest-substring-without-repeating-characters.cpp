a78d2281f4d6aadb53d1e5ce863fcf8773711c0c
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2023-06-21 22:11:24 +0530

    Time: 8 ms (88.28%), Space: 7.5 MB (87.81%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,16 +1,35 @@
-class Solution {
+class Solution 
+{
 public:
-    int lengthOfLongestSubstring(string s) {
-        vector<int> mpp(256,-1);
-        int left=0,right=0;
-        int len=0;
-        int n=s.size();
-        while(right<n){
-            if(mpp[s[right]]!=-1) left=max(mpp[s[right]]+1,left);
-            mpp[s[right]]=right;
-            len=max(len,right-left+1);
-            right++;
-        } 
-        return len;
-    } 
+    int lengthOfLongestSubstring(string s) 
+    {
+        int max_len = 0;
+        
+        // [1] longest substring is the one with the largest 
+        //    difference between positions of repeated characters; 
+        //    thus, we should create a storage for such positions 
+        vector<int> pos(128,0);
+        
+        // [2] while iterating through the string (i.e., moving 
+        //    the end of the sliding window), we should also 
+        //    update the start of the window 
+        int start = 0;
+        
+        for (int end = 0; end < s.size(); end++)
+        {
+            auto ch = s[end];
+            
+            // [3] get the position for the start of sliding window 
+            //    with no other occurences of 'ch' in it 
+            start = max(start, pos[ch]);
+            
+            // [4] update maximum length 
+            max_len = max(max_len, end-start+1);
+            
+            // [5] set the position to be used in [3] on next iterations
+            pos[ch] = end + 1;
+        }
+                
+        return max_len;
+    }
 };
