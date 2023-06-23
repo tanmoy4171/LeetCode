f2ae46651fc74b5df3e969bc7f7aa8e255abcd9e
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2023-06-23 18:02:36 +0530

    Time: 172 ms (94.35%), Space: 13.8 MB (96.45%) - LeetHub

---
+++ 1087-longest-arithmetic-subsequence/1087-longest-arithmetic-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+   int dp[1001][1001];
+     int longestArithSeqLength(vector<int>& nums) {
+        memset(dp , 0 , sizeof(dp));
+         int n=nums.size();
+         int maxlen =1;
+         for(int i=1 ; i<n ;i++)
+         {
+             for(int j=0;j<i;j++)
+             {
+                 int diff=nums[i]-nums[j] + 500;  // making the diff positive as it may be negative , " no negative indexing "
+                 
+                 dp[i][diff] = max(dp[i][diff] , 1 + dp[j][diff]);
+                 
+                 maxlen = max(maxlen , dp[i][diff]);
+                 
+             }
+         }
+         
+         return maxlen + 1;
+    }
+};
