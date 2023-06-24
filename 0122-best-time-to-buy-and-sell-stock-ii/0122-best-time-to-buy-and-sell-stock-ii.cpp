0da79a079e087d203fb095c476c04d82bb4dbd8a
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2023-06-24 14:38:39 +0530

    Time: 11 ms (44.37%), Space: 13 MB (79.21%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans=0,profit=0;
+        int valley,peak;
+        for(int i=0;i<prices.size();i++)
+        {
+            while(i<prices.size()-1 && prices[i]>=prices[i+1])i++;
+            valley = prices[i];
+            while(i<prices.size()-1 && prices[i]<=prices[i+1])i++;
+            peak = prices[i];
+            profit += peak-valley;
+        }
+        return profit;
+    }
+};
