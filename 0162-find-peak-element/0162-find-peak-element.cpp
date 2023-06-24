c29421b88c82cab24d367bc6855e6251a4e96a41
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2023-06-24 14:33:37 +0530

    Time: 0 ms (100%), Space: 8.8 MB (55.33%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        while (l < r) 
+            if (nums[l] < nums[r]) ++l;
+            else --r;
+        return r;
+    }
+};
