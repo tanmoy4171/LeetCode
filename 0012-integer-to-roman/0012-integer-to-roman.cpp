54a4cebbee379fcb98c921d8aeca3926e5d2a844
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2023-07-02 23:44:02 +0530

    Time: 3 ms (94.85%), Space: 9.3 MB (22.75%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,12 +1,20 @@
-class Solution {
+class Solution 
+{
 public:
-    const int val[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-    const string rom[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-
-    string intToRoman(int N) {
-        string ans = "";
-        for (int i = 0; N; i++)
-            while (N >= val[i]) ans += rom[i], N -= val[i];
-        return ans;
+    string intToRoman(int num) 
+    {
+        vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},{100, "C"},{90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"},{5, "V"}, {4, "IV"}, {1, "I"}};
+        
+        string res="";
+        
+        for(auto it:roman)
+        {
+            while(num >= it.first)
+            {
+                res += it.second;
+                num -= it.first;
+            }
+        }
+        return res;
     }
 };
