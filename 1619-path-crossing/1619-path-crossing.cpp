28d72ce9e9b9e89db5c1cdb2d5c6ad77d00cec1c
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2023-07-07 17:28:33 +0530

    Time: 7 ms (9.54%), Space: 7.2 MB (8.51%) - LeetHub

---
+++ 1619-path-crossing/1619-path-crossing.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        int a = 0, b = 0;;
+        set<vector<int>> x;
+        x.insert({0, 0});
+        for (char i: path){
+            if (i == 'N'){
+                b++;
+            } else if (i == 'E'){
+                a++;
+            } else if (i == 'S'){
+                b--;
+            } else {
+                a--;
+            }
+            if (x.find({a, b}) != x.end()){
+                return true;
+            }
+            x.insert({a, b});
+        }
+       
+        return false;
+    }
+};
