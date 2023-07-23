99f27c4fb2fd92fa4573c194ce479245d9830d1f
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2023-07-23 13:48:35 +0530

    Added solution - LeetHub

---
+++ Egg Dropping Puzzle - GFG/egg-dropping-puzzle.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find minimum number of attempts needed in 
+    //order to find the critical floor.
+    int eggDrop(int k, int n) 
+    {
+       vector<int> dp(k + 1, 0);
+        int moves = 0;
+        while(dp[k] < n) {
+            moves++;
+            for(int i = k; i > 0; i--) dp[i] += 1 + dp[i-1];
+        }
+        return moves;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking eggs and floors count
+        int n, k;
+        cin>>n>>k;
+        Solution ob;
+        //calling function eggDrop()
+        cout<<ob.eggDrop(n, k)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
