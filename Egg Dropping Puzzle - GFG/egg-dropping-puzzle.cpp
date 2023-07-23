68b1f96adeffb6d4991a60716a691c2b8ca9211b
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2023-07-23 15:39:59 +0530

    Added solution - LeetHub

--- Egg Dropping Puzzle - GFG/egg-dropping-puzzle.cpp
+++ Egg Dropping Puzzle - GFG/egg-dropping-puzzle.cpp
@@ -8,15 +8,39 @@ class Solution
     public:
     //Function to find minimum number of attempts needed in 
     //order to find the critical floor.
-    int eggDrop(int k, int n) 
-    {
-       vector<int> dp(k + 1, 0);
-        int moves = 0;
-        while(dp[k] < n) {
-            moves++;
-            for(int i = k; i > 0; i--) dp[i] += 1 + dp[i-1];
+    int solve(int e,int f,vector<vector<int> > &dp ){
+        
+        if(f==0 || f==1)return f;
+        if(e==1)return f;
+        int mn=1e9;
+        if(dp[e][f]!=-1)return dp[e][f];
+        for(int k=1;k<=f;k++){
+            int low=-1e9,high=-1e9;
+            if(dp[e-1][k-1]!=-1){
+                low=dp[e-1][k-1];
+            }
+            else{
+                low=solve(e-1,k-1,dp);
+                dp[e-1][k-1]=low;
+            }
+            if(dp[e][f-k]!=-1){
+                high=dp[e][f-k];
+            }
+            else{
+                high=solve(e,f-k,dp);
+                dp[e][f-k]=high;
+            }
+            int temp=1+max(low,high);
+            mn=min(temp,mn);
         }
-        return moves;
+        return dp[e][f] = mn;
+    }
+    int eggDrop(int e, int f) 
+    {
+        // your code here
+        vector<vector<int> > dp (e+1,vector<int> (f+1,-1));
+        return solve(e,f,dp);
+        
     }
 };
 
