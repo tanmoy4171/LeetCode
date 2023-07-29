5f0437eebf5346996bd1654744fa3c50e87df873
TANMOY HALDER <107719903+tanmoy4171@users.noreply.github.com>
2023-07-29 10:56:46 +0530

    Time: 0 ms (100%), Space: 5.8 MB (88.8%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int reverse(int x) {
-        int reverse = 0 ; 
+        int reverse = 0; 
         while(x!=0){
             int lastdigit = x%10 ; 
             if(reverse > INT_MAX/10 || reverse < INT_MIN/10){
